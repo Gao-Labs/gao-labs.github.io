89d14f020c237c8dd8ad88650d19573f673896a6
Erin Murphy <egm77@cornell.edu>
2023-11-07 14:09:27 -0500

    Add cards and pics to bio page

--- website/src/app/bios/bios.component.ts
+++ website/src/app/bios/bios.component.ts
@@ -3,24 +3,77 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bios',
   templateUrl: './bios.component.html',
-  styleUrls: ['./bios.component.css']
+  styleUrls: ['./bios.component.css'],
 })
 export class BiosComponent {
-  people: any[] = [
+  imageDir = '../../assets/headshots/';
+
+  coreStaff: any[] = [
     {
-      name: 'John Doe',
-      bio: 'John Doe is a professor at our university. He specializes in...',
-      // Add more properties like 'image' for headshots if you have image URLs.
+      name: 'Dr. H. Oliver Gao',
+      title: 'Professor etc',
+      bio: 'Dr. Gao is the Director of the Cornell Systems Engineering and Howard Simpson Professor of Engineering at Cornell University. Gao is an international leading expert in urban infrastructure, transportation, and health (environmental  health, public health) systems analytics/modeling and innovation for healthy living in smart communities. His research focuses on urban technology, data science, integrated computational engineering models, and trans-disciplinary systems solutions for intelligent urban systems, low carbon and low emission transportation, and human-centered urban design for environment and public health. He is an elected member in the graduate fields of 1) Civil and Environmental Engineering, 2) Computer Science and Engineering; 3) Air Quality in Earth and Atmospheric Science, and 4) Systems Engineering at Cornell University.',
+      image: this.imageDir + 'oliver-group.jpg',
     },
     {
-      name: 'Jane Smith',
-      bio: 'Jane Smith is a postdoctoral researcher working on...',
+      name: 'Dr. Tim Fraser',
+      title: 'Postdoc etc',
+      bio: 'bio bio bio',
+      image: this.imageDir + 'Fraser_headshot_2022 - 560x560.jpg',
+    },
+    {
+      name: 'Erin Murphy',
+      title: 'Program Analyst',
+      bio: 'Erin is one of the staff developers at Gao Labs.',
+      image: this.imageDir + 'erin-headshot.png',
     },
     {
       name: 'Bob Johnson',
-      bio: 'Bob Johnson is an associate professor focusing on...',
+      title: 'PhD Student',
+      bio: 'Bob Johnson is a PhD student focusing on...',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    },
+    {
+      name: 'Bob Johnson',
+      title: 'PhD Student',
+      bio: 'Bob Johnson is a PhD student focusing on...',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    },
+    {
+      name: 'Bob Johnson',
+      title: 'PhD Student',
+      bio: 'Bob Johnson is a PhD student focusing on...',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
     },
     // Add more people as needed
   ];
 
+  phdStudents: any[] = [
+    {
+      name: 'Bob Johnson',
+      title: 'PhD Student',
+      bio: 'Bob Johnson is a PhD student focusing on...',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    },
+    {
+      name: 'Bob Johnson',
+      title: 'PhD Student',
+      bio: 'Bob Johnson is a PhD student focusing on...',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    },
+  ];
+  mastersStudents: any[] = [
+    {
+      name: 'Bob Johnson',
+      title: 'PhD Student',
+      bio: 'Bob Johnson is a PhD student focusing on...',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    },
+    {
+      name: 'Bob Johnson',
+      title: 'PhD Student',
+      bio: 'Bob Johnson is a PhD student focusing on...',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    },
+  ];
 }
--- website/src/app/bios/bios.module.ts
+++ website/src/app/bios/bios.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { BiosComponent } from './bios.component';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [BiosComponent],
-  imports: [MatCardModule, CommonModule],
+  imports: [MatCardModule, CommonModule, MatButtonModule],
   exports: [BiosComponent],
 })
 export class BiosModule {}
