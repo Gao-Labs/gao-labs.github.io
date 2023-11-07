4c4b6078a2d749e09b887ab212cd379c63fde9f5
Erin Murphy <egm77@cornell.edu>
2023-11-07 09:43:33 -0500

    Fix bios not displaying

--- website/src/app/bios/bios.module.ts
+++ website/src/app/bios/bios.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { BiosComponent } from './bios.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [BiosComponent],
-  imports: [MatCardModule],
-  exports: [BiosComponent]
+  imports: [MatCardModule, CommonModule],
+  exports: [BiosComponent],
 })
-export class BiosModule { }
+export class BiosModule {}
