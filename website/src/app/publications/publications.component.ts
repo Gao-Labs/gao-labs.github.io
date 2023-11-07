4be3cc32b3571ff8c30426f743270601d61c9317
Erin Murphy <egm77@cornell.edu>
2023-11-07 14:09:52 -0500

    Implement publications page

--- website/src/app/publications/publications.component.ts
+++ website/src/app/publications/publications.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-publications',
   templateUrl: './publications.component.html',
-  styleUrls: ['./publications.component.css']
+  styleUrls: ['./publications.component.css'],
 })
 export class PublicationsComponent {
-
+  pubs: any[] = [
+    {
+      title: 'New emissions calculator at Cornell University is very cool',
+      link: '#',
+      authors: 'dr gao',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: 'can you believe how cool this is??',
+    },
+    {
+      title: 'New emissions calculator at Cornell University is very cool',
+      link: '#',
+      authors: 'dr gao',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: 'can you believe how cool this is??',
+    },
+    {
+      title: 'New emissions calculator at Cornell University is very cool',
+      link: '#',
+      authors: 'dr gao',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: 'can you believe how cool this is??',
+    },
+  ];
 }
--- website/src/app/publications/publications.module.ts
+++ website/src/app/publications/publications.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PublicationsComponent } from './publications.component';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [PublicationsComponent],
   exports: [PublicationsComponent],
+  imports: [MatCardModule, CommonModule, MatButtonModule],
 })
 export class PublicationsModule {}
