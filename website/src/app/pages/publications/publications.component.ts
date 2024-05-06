231d94eb95291b737d895d5e346c044d2924f5a6
Erin Murphy <egm77@cornell.edu>
2024-05-06 13:31:02 -0400

    Add publications from 2018

--- website/src/app/pages/publications/publications.component.ts
+++ website/src/app/pages/publications/publications.component.ts
@@ -7,26 +7,26 @@ import { Component } from '@angular/core';
 })
 export class PublicationsComponent {
   pubs: any[] = [
-    {
-      title: 'New emissions calculator at Cornell University is very cool',
-      link: '#',
-      authors: 'dr gao',
-      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-      description: 'can you believe how cool this is??',
-    },
-    {
-      title: 'New emissions calculator at Cornell University is very cool',
-      link: '#',
-      authors: 'dr gao',
-      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-      description: 'can you believe how cool this is??',
-    },
-    {
-      title: 'New emissions calculator at Cornell University is very cool',
-      link: '#',
-      authors: 'dr gao',
-      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-      description: 'can you believe how cool this is??',
-    },
+    // {
+    //   title: 'New emissions calculator at Cornell University is very cool',
+    //   link: '#',
+    //   authors: 'dr gao',
+    //   image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    //   description: 'can you believe how cool this is??',
+    // },
+    // {
+    //   title: 'New emissions calculator at Cornell University is very cool',
+    //   link: '#',
+    //   authors: 'dr gao',
+    //   image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    //   description: 'can you believe how cool this is??',
+    // },
+    // {
+    //   title: 'New emissions calculator at Cornell University is very cool',
+    //   link: '#',
+    //   authors: 'dr gao',
+    //   image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    //   description: 'can you believe how cool this is??',
+    // },
   ];
 }
